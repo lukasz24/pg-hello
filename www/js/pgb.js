6899c96b969f8403ae1ef5e8d3058aa130892c6f
lukasz24 <lukasz.pudzisz@gmail.com>
2018-03-11 13:44:28 +0100

    Device Movement

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -4,7 +4,28 @@ function init() {
 
 function onDeviceReady() {
 	navigator.notification.beep(1);
+
+	function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+    navigator.notification.alert('TEST Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
 }
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 3000 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+
+
 
 function deviceInfo() {
 
