0fc4b3ac3a6310a4a4759b649d0158535e588f9e
lukasz24 <lukasz.pudzisz@gmail.com>
2018-03-11 13:48:12 +0100

    Device movement
    Remove one alert

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -4,16 +4,14 @@ function init() {
 
 function onDeviceReady() {
 	navigator.notification.beep(1);
-
+	//DEVICE MOVEMENT
+/*
 	function onSuccess(acceleration) {
     alert('Acceleration X: ' + acceleration.x + '\n' +
           'Acceleration Y: ' + acceleration.y + '\n' +
           'Acceleration Z: ' + acceleration.z + '\n' +
           'Timestamp: '      + acceleration.timestamp + '\n');
-    navigator.notification.alert('TEST Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
+
 }
 
 function onError() {
@@ -23,6 +21,7 @@ function onError() {
 var options = { frequency: 3000 };  // Update every 3 seconds
 
 var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+*/
 }
 
 
