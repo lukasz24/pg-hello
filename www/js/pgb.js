740fbc1f6aea2c2db29a32c9823c17aac0b9549c
lukasz24 <lukasz.pudzisz@gmail.com>
2018-03-12 14:01:50 +0100

    .

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -9,11 +9,24 @@ function onDeviceReady() {
 	document.addEventListener("offline", checkConnection, false);
 	document.addEventListener("online", checkConnection, false);
 
-	window.addEventListener("compassneedscalibration", function(event) {
-        event.preventDefault();
-    }, true);
-    detectMotion();
-    window.removeEventListener("devicemotion", processEvent);
+	function onSuccess(acceleration) {
++    alert('Acceleration X: ' + acceleration.x + '\n' +
++          'Acceleration Y: ' + acceleration.y + '\n' +
++          'Acceleration Z: ' + acceleration.z + '\n' +
++          'Timestamp: '      + acceleration.timestamp + '\n');
++    navigator.notification.alert('TEST Acceleration X: ' + acceleration.x + '\n' +
++          'Acceleration Y: ' + acceleration.y + '\n' +
++          'Acceleration Z: ' + acceleration.z + '\n' +
++          'Timestamp: '      + acceleration.timestamp + '\n');
++}
++
++function onError() {
++    alert('onError!');
++}
++
++var options = { frequency: 3000 };  // Update every 3 seconds
++
++var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 
 }
 
